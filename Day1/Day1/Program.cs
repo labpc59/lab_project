ba25fd3161d03690b6ed14620a0986c33d4839ff
Abhijeet-patil-cpu <patilabhijeetnarendra@gmail.com>
2024-07-07 15:36:59 +0530

    day1

--- Day1/Day1/Program.cs
+++ Day1/Day1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using n1;
 using n1.n1_1;
 using System;
+
 namespace Day1
 {
     internal class Program
@@ -8,55 +9,107 @@ namespace Day1
         static void Main(string[] args)
         {
             Console.WriteLine("Main");
-            Class1 o;
-            o=new Class1();
-            o.MC1();
-            Class2 a = new Class2();
-            a.MC2();
-            Class4 result=new Class4();
-            result.add(4, 5, 3);
-            result.add(c:4);
+
+            // Example usage of Class7
+            Class7 class7 = new Class7();
+            class7.I = 11;
+            Console.WriteLine(class7.I);
+
+            // Set class7 to null and call GC.Collect
+            class7 = null;
+
+            // Force garbage collection and wait for finalizers
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            // Adding a longer delay to ensure finalization
+            System.Threading.Thread.Sleep(1000); // 1 second delay
+
+            Console.WriteLine("Garbage collection complete");
         }
     }
 }
-namespace n1 {
 
-    public class Class1 {
-            public void MC1 ()
+namespace n1
+{
+    public class Class1
+    {
+        public void MC1()
         {
             Console.WriteLine("class 1");
         }
-             
-}
-    public class Class2 {
+    }
+
+    public class Class2
+    {
         int x = 100;
-        public void MC2() 
+        public void MC2()
         {
             Console.WriteLine("class 2");
             Console.WriteLine(++x);
         }
     }
+
     namespace n1_1
     {
-        class Class3 
+        class Class3
         {
             public void MC3()
             {
                 Console.WriteLine("class 3");
-
             }
         }
 
         class Class4
         {
-            public void add(int a= 0, int b=0,int c=0)
+            public void add(int a = 0, int b = 0, int c = 0)
             {
                 int x = a + b + c;
                 Console.WriteLine(x);
-
             }
         }
 
-    }
+        class Class5
+        {
+            int i = 0;
+            public void setI(int i)
+            {
+                this.i = i;
+            }
+            public int getI()
+            {
+                return i;
+            }
+        }
 
+        class Class6
+        {
+            int i = 0;
+            public int I
+            {
+                set
+                {
+                    if (value < 10)
+                    {
+                        i = value;
+                    }
+                    else Console.WriteLine("EXCEPTION");
+                }
+                get
+                {
+                    return i;
+                }
+            }
+        }
+
+        public class Class7
+        {
+            public int I { get; set; }
+
+            ~Class7()
+            {
+                Console.WriteLine("inside destructor");
+            }
+        }
+    }
 }
---
+++ Day1/Day1_1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics.Metrics;
+using System;
+
+namespace Day1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            class1 Class1=new class1();
+            Class1.Display();
+            class1.DisplayStaic();
+        }
+
+
+        public class class1 
+        {
+            public  int h;
+            public static int i;
+
+        public static int j=5;
+            public void Display() 
+            {
+                System.Console.WriteLine(h);
+                System.Console.WriteLine(i);//from nonStatic function we can access static members 
+                System.Console.WriteLine(j);
+
+            }
+            public static void DisplayStaic()
+            {
+                 //System.Console.WriteLine(h);//but from staicfunction we can not access non Static members 
+                  //Error   An object reference is required for the non-static field, method, or property 'Program.class1.h' 
+                 //so here conclusion by static function we can onely access static member 
+                System.Console.WriteLine(i);
+                System.Console.WriteLine(j);
+
+            }
+        }
+        public static class Class2 
+        {
+            private static int i ;//compolsory to make it as staic cause if not it will not goin to access by static property 
+            public static int I 
+            {
+                set {
+                    i=value;
+                }
+                get { return i; }
+            }
+        }
+    }
+}
+// when static constructor get called ans: when class get loaded in to the memory 
+//when class get loaded into memory ans: when the first object is created or static member is access first time
