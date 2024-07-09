a665f051f00ee95786037e43de2427b53d658d56
tulsimundada <tulsimundada@gmail.com>
2024-07-10 02:14:53 +0530

    cdac1

--- Day1/day1_Inheritance/Program.cs
+++ Day1/day1_Inheritance/Program.cs
@@ -5,7 +5,38 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.ReadLine();
+             DerivedClass derivedClass =new DerivedClass();
+            derivedClass.display();
+            SubDerivedClass subDerivedClass = new SubDerivedClass();
+            subDerivedClass.display(); // Display method from DerivedClass
+        }
+
+        public abstract class BaseClass
+        {
+            protected private int i = 19;
+           // public int i1 { get; set; }
+        }
+
+        public abstract class DerivedClass : BaseClass
+            {
+
+           public abstract void display() { 
+           
+                Console.WriteLine(i);
+            }
+
+            }
+
+        public class SubDerivedClass : DerivedClass
+        {
+
+             
+                public override void display()
+            {
+
+                Console.WriteLine("hi"+i);
+            }
+
         }
     }
 }
