74f7c42c1767e9f1b276f0dd9fa746d89825635f
ShreyasDamase <shreyasdamase@gmail.com>
2024-07-13 10:50:45 +0530

    home

---
+++ Exception/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+
+namespace Exception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            while (true) 
+            {
+            
+            try
+            {
+                Console.WriteLine("Hello, World!");
+                int a = int.Parse(Console.ReadLine());
+                Console.WriteLine(a);
+                    break;
+            }
+            catch {
+                Console.WriteLine("Exception occure");
+
+            }
+            }
+        }
+        
+    }
+}
